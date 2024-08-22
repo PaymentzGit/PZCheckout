b7579fc8f89a80bb77042ef028260eee41083049
shubham.tandale <shubham.tandale@paymentz.com>
2024-08-22 19:01:32 +0530

    Merge issues.

--- app/src/main/java/com/paymentz/pz_checkout_sdk/model/PZCheckout.java
+++ app/src/main/java/com/paymentz/pz_checkout_sdk/model/PZCheckout.java
@@ -1,13 +1,9 @@
 package com.paymentz.pz_checkout_sdk.model;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import com.paymentz.pz_checkout_sdk.WebCheckoutPayment;
-
 import java.io.Serializable;
-import java.security.MessageDigest;
 
 /**
  * Created by Admin on 8/23/2018.
@@ -20,23 +16,12 @@ public class PZCheckout implements Serializable {
 
     private PZCheckoutListener pzCheckoutListener;
 
-    public void initPayment(Activity activity, RequestParameters requestParameters) {
+    public void initPayment(Activity activity, PayRequest requestParameters) {
         Intent intent = new Intent(activity,WebCheckoutPayment.class);
         intent.putExtra("requestParameters",requestParameters);
         activity.startActivityForResult(intent,PAYMENT_REQUEST_CODE);
     }
 
-    /*@Override
-    public void onSuccessful(PayResult payResult) {
-        System.out.println("onSuccess::::");
-        pzCheckoutListener.onSuccess(payResult);
-    }
-
-    @Override
-    public void onFail(String title, String message) {
-        System.out.println("onFail::::");
-        pzCheckoutListener.onFailure(title, message);
-    }*/
 
     public interface PZCheckoutListener {
         void onSuccess(PayResult payResult);
